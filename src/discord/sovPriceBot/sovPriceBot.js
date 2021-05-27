f6c123f974bc3f35690df9db11633520e52c815e
Neo <83775942+neo-dr@users.noreply.github.com>
2021-05-27 07:05:19 -0700

    Better way to get SOV in USD

--- src/discord/sovPriceBot/sovPriceBot.js
+++ src/discord/sovPriceBot/sovPriceBot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 import axios from 'axios';
+import Web3 from 'web3';
+import abiPriceFeed from "../../../config/ABI/abiPriceFeed.json";
 const moment = require('moment-timezone');
 const config = require('config'),
   _ = require('lodash'),
@@ -86,7 +88,7 @@ class DiscordPriceBotCtrl {
           priceData = await fetchCurrentPrice();
           lastRan = moment.tz('UTC').format('LL LTS');
           const price = priceData.price * 100000000,
-            sovUsdPrice = await getSovUsdPrice(),
+            sovUsdPrice = await getSovUsdPrice(priceData.price),
             sovMarketCap = await getSovMarketCap(sovUsdPrice);
           // Update bot nickname with price
 
@@ -120,15 +122,26 @@ async function fetchCurrentPrice() {
   return response.data;
 }
 
-async function getSovUsdPrice() {
-  // TODO: how do i create this?
-  const startTime = 1622075400000,
-    // sovUsdPrice can be fed params: ?startTime=1622075400000
-    sovUsdPricing = await axios.get(
-      `${config.urls.sovUsdPrice}?startTime=${startTime}`
-    );
-  // console.log(startTime);
-  return _.last(sovUsdPricing.data.series)['close'];
+async function getSovUsdPrice(sovPrice) {
+    const mainNetContracts = {
+        BTC_token: "0x542fDA317318eBF1d3DEAf76E0b632741A7e677d",
+        USDT_token: "0xEf213441a85DF4d7acBdAe0Cf78004E1e486BB96",
+        priceFeed: "0x437AC62769f386b2d238409B7f0a7596d36506e4",
+    },
+    web3 = new Web3("https://mainnet.sovryn.app/rpc"),
+    contractPriceFeed = new web3.eth.Contract(abiPriceFeed, mainNetContracts.priceFeed);
+    let price;
+    try {
+        price = await contractPriceFeed.methods.queryRate(
+            Web3.utils.toChecksumAddress(mainNetContracts.BTC_token),
+            Web3.utils.toChecksumAddress(mainNetContracts.USDT_token) 
+            ).call();
+        const btcPrice = parseFloat(Web3.utils.fromWei(price.rate)).toFixed(6);
+        return btcPrice * sovPrice;
+    } catch(e) {
+        console.error(e)
+    }
+    return undefined;
 }
 
 async function getSovMarketCap(currentPrice) {
